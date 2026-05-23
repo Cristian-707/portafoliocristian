1f1eaf26a02bece1a81db4a63877e4ee2339a001
Cristian-707 <cfub1999@gmail.com>
2026-05-23 16:19:41 -0600

    tarea completada

---
+++ portafolio.js
@@ -0,0 +1,7 @@
+ function mostrarMensaje(){
+      alert("Gracias por visitar mi portafolio web 🚀");
+    }
+ 
+       function modoOscuro(){
+      document.body.classList.toggle("dark-mode");
+    }
